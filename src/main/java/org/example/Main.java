bdb003cd005866f9cd11a5329cfc45d17f959568
Natalya Bochkova <bochkovanatalya7@gmail.com>
2022-11-14 17:39:04 +0300

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello qa.quru!");
-        System.out.println("My name is Natali");
+        System.out.println("My name is Natalyy");
         }
     }
