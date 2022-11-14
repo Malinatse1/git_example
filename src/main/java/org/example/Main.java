1c50cf172da7e3bc519591208d847dc316f7d08d
Natalya Bochkova <bochkovanatalya7@gmail.com>
2022-11-14 17:25:39 +0300

    My name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.quhru1!");
+        System.out.println("Hello qa.quru!");
+        System.out.println("My name is Nataly");
+        }
     }
-}
